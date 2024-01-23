68cd83a2ca185e99737467c8a3aad86943a90c64
farhanasidik12@gmail.com <farhanasidik12@gmail.com>
2024-01-23 22:23:37 +0700

    master:menambahkan save dan update data controller

--- backend/app/Http/Controllers/Siswa.php
+++ backend/app/Http/Controllers/Siswa.php
@@ -68,7 +68,76 @@ class Siswa extends Controller
             $message = "Data Gagal Dihapus ! (id TIDAK DITEMUKAN !)";
         }
 
-        return response(["status" => $status, "message" => $message], http_response_code());
-   }
+        // buat fungsi untuk simpan data 
+    function saveController(Request $req)
+    {
+        // ambil data
+        // "nis" = variabel array yang menampung nilai dari $req
+        // "$req->nis" = variabel yang dikirim dari front end
+        $data = [
+            "nis" => $req->nis,
+            "nama" => $req->nama,
+            "email" => $req->email,
+            "telfon" => $req->telfon,
+            // "id" => base64_encode(md5($req->npm));
+        ];
+        
+        $id = base64_encode(md5($req->nis));
 
+        // lakukan pengecekan apakah data "npm" yang diisi sudah pernah tersimpan/belum di database
+        // jika data tersedia
+        if(count($this->model->detailData($id)) == 1)
+        {
+            
+            // buat status dan pesan
+            $status = 0;
+            $message = "Data Gagal Tersimpan NPM Sudah Ada";
+        }
+        // jika data tidak tersedia
+        else
+        {
+            $this->model->saveData($data["nis"],$data["nama"],$data["email"],$data["telfon"]);
+            // buat status dan pesan
+            $status = 1;
+            $message = "Data Berhasil Disimpan";
+        }
+        // Kembalikan nilai variabel "result" ke dalam object "siswa"
+        return response(["status" => [$status],"message" => $message], http_response_code());
+    }
+
+    // buat fungsi untuk ubah data
+    function updateController(Request $req, $id)
+    {
+        // ambil data input
+        $data = [
+            "nis" => $req->nis,
+            "nama" => $req->nama,
+            "email" => $req->email,
+            "telfon" => $req->telfon
+       
+        ];
+        // set nilai id
+        // $id = base64_encode(md5($req->nis));
+
+        // lakukan pengecekan apakah data "nis" yang diisi sudah pernah tersimpan/belum di database
+        if(count($this->model->checkUpdateData($data["nis"], $id)) == 0)
+        {
+            // lakukan perubahan data 
+            // panggilan fungsi updateData dari model "siswa"
+            $this->model->updateData($data
+            ["nis"],$data["nama"],$data
+            ["email"],$data["telfon"],
+            $id);
+
+            $status = 1;
+            $message = "Data Berhasil Diubah";
+        }
+        // jika data tidak tersedia
+        else
+        {
+            $status = 0;
+            $message = "Data Gagal Diubah (nis tidak ditemukan)";
+
+        return response(["status" => $status, "message" => $message], http_response_code());
+    }
 }
