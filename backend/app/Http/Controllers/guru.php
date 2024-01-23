213698a6f451332d18df6dce7ed89c8897b44449
farhanasidik12@gmail.com <farhanasidik12@gmail.com>
2024-01-23 23:27:25 +0700

    master:menambahkan save dan update data pada model guru

--- backend/app/Http/Controllers/guru.php
+++ backend/app/Http/Controllers/guru.php
@@ -70,5 +70,80 @@ class Guru extends Controller
 
         return response(["status" => $status, "message" => $message], http_response_code());
    }
+         // buat fungsi untuk simpan data 
+         function saveController(Request $req)
+         {
+             // ambil data
+             // "id" = variabel array yang menampung nilai dari $req
+             // "$req->id" = variabel yang dikirim dari front end
+             $data = [
+                 "id" => $req->id,
+                 "nama" => $req->nama,
+                 "email" => $req->email,
+                 "status" => $req->status,
+                 "telfon" => $req->telfon,
+                 // "id" => base64_encode(md5($req->npm));
+             ];
+             
+             $id = base64_encode(md5($req->nis));
+     
+             // lakukan pengecekan apakah data "id" yang diisi sudah pernah tersimpan/belum di database
+             // jika data tersedia
+             if(count($this->model->detailData($id)) == 1)
+             {
+                 
+                 // buat status dan pesan
+                 $status = 0;
+                 $message = "Data Gagal Tersimpan id Sudah Ada";
+             }
+             // jika data tidak tersedia
+             else
+             {
+                 $this->model->saveData($data["id"],$data["nama"],$data["email"],$data["status"]$data["telfon"]);
+                 // buat status dan pesan
+                 $status = 1;
+                 $message = "Data Berhasil Disimpan";
+             }
+             // Kembalikan nilai variabel "result" ke dalam object "guru"
+             return response(["status" => [$status],"message" => $message], http_response_code());
+         }
+     
+         // buat fungsi untuk ubah data
+         function updateController(Request $req, $id)
+         {
+             // ambil data input
+             $data = [
+                 "id" => $req->id,
+                 "nama" => $req->nama,
+                 "email" => $req->email,
+                 "status" => $req->status,
+                 "telfon" => $req->telfon
+            
+             ];
+             // set nilai id
+             // $id = base64_encode(md5($req->id));
+     
+             // lakukan pengecekan apakah data "id" yang diisi sudah pernah tersimpan/belum di database
+             if(count($this->model->checkUpdateData($data["nis"], $id)) == 0)
+             {
+                 // lakukan perubahan data 
+                 // panggilan fungsi updateData dari model "guru"
+                 $this->model->updateData($data
+                 ["id"],$data["nama"],$data
+                 ["email"],$data["status"],$data["telfon"]
+                 $id);
+     
+                 $status = 1;
+                 $message = "Data Berhasil Diubah";
+             }
+             // jika data tidak tersedia
+             else
+             {
+                 $status = 0;
+                 $message = "Data Gagal Diubah (nis tidak ditemukan)";
+             }
+             return response(["status" => $status, "message" => $message], http_response_code());
+    
+        }        
 
 }
