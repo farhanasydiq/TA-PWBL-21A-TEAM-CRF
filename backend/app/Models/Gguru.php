eab2dae2bdd126227fff26b7708940e07ed2c38c
farhanasidik12@gmail.com <farhanasidik12@gmail.com>
2024-01-23 23:06:32 +0700

    master:menambahkan save dan update data model

--- backend/app/Models/Gguru.php
+++ backend/app/Models/Gguru.php
@@ -12,7 +12,7 @@ class Gguru extends Model
    function getData()
    {
     $query = DB::table('tb_guru')
-            ->select("id AS id_guru","nama AS nama_guru","email","telepon AS number_phone")
+            ->select("id AS id_guru","nama AS nama_guru","email as email_guru","telfon AS number_phone","status AS status_guru")
             ->orderBy("id")
             ->get();
 
@@ -23,13 +23,14 @@ class Gguru extends Model
    {
     // tampilkan data dari "tb_guru"
     $query = DB::table('tb_guru')
-            ->select("id AS id_guru","nama AS nama_guru","email","telepon AS number_phone")
+            ->select("id AS id_guru","nama AS nama_guru","email as email_guru","telfon AS number_phone","status AS status_guru")
             ->where("id","$keyword")
             //->orWhereRaw("REPLACE(nama,' ','') LIKE REPLACE('%$keyword%',' ','')")
             //->orWhere("nama","LIKE","%$keyword%")
             ->orWhere(DB::raw("REPLACE(nama,' ','')"),"LIKE",DB::raw("REPLACE('%$keyword%',' ','')"))
-            ->orWhere("telepon","LIKE","$keyword")
+            ->orWhere("telfon","LIKE","$keyword")
             ->orWhere("email","LIKE","%$keyword%")
+            ->orWhere("status","LIKE","%$keyword%")
             ->orderBy("id")
             ->get();
     
@@ -40,13 +41,61 @@ class Gguru extends Model
    // funsi detail data
    function detailData($id)
    {
-         // tampilkan data dari "tb_mahasiswa"
+         // tampilkan data dari "tb_guru"
     $query = DB::table('tb_guru')
-    ->select("id AS id_guru","nama AS nama_guru","email","telepon AS number_phone")
+    ->select("id AS id_guru","nama AS nama_guru","email as email_guru","telfon AS number_phone","status AS status_guru")
     ->where(DB::raw("TO_BASE64(MD5(id))"),"$id")
     ->get();
 
 // mengirim hasil variabel "query" ke controller "Guru"
 return $query;
    }
+
+   // buat fungsi untuk simpan data
+   function saveData($id,$nama,$email,$telfon,$status)
+   {
+       //ambil data
+       $result = [
+           "id" => $id,
+           "nama" => $nama,
+           "email" => $email,
+           "status" => $status,
+           "telfon" => $telfon,
+       ];
+       // perintah simpan data
+       DB::table("tb_guru")
+           ->insert($result);
+   }
+   function checkUpdateData($npm,$id)
+   {
+       $query = DB::table('tb_guru')
+       ->select("id as id_guru","nama as nama_ guru","email as email_guru","telfon as number_phone",
+       "status as status_guru")
+       // ->where(DB::raw("TO_BASE64(MD5(id))"),"$id")
+       ->where(DB::raw("TO_BASE64(MD5(id))"), "!=", "$id")
+       ->where("id", "$id")
+       ->get();
+
+   // mengirim hasil variabel "query" ke controller "guru"
+   return $query;
+   }
+
+       //fungsi ubah data 
+       function updateData ($id, $nama, $email, $telfon, $status)
+       {
+             //ambil data
+             $result = [
+               "id" => $id,
+               "nama" => $nama,
+               "email" => $email,
+               "status" => $status,
+               "telfon" => $telfon,
+           ];
+   
+           // perintah untuk ubah data
+           DB::table("tb_guru")
+           ->where(DB::raw("TO_BASE64(MD5(id))"),"$id")
+           ->update($result);
+       }
+
 }
